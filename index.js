acabb948d89d7bf25543e1cee62096a87a4adb4e
Agnes Nowak <kesse8401@hotmail.com>
2018-07-03 12:05:55 +0200

    Test no4 #1

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ function calculateSupply(age, amountPerDay) {
     return "You will need " + amountPerDay + " to last you until the ripe old age of " + maxAge;
 };
 
-document.getElementById("g").innerHTML = calculateSupply(1, 30);
-document.getElementById("h").innerHTML = calculateSupply(2, 40);
-document.getElementById("i").innerHTML = calculateSupply(3, 50);
+document.getElementById("g").innerHTML = calculateSupply(50, 1);
+document.getElementById("h").innerHTML = calculateSupply(60, 2);
+document.getElementById("i").innerHTML = calculateSupply(70, 3);
