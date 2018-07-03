fadabfa675325fb8adb1be11651df1b2f7910c0e
Agnes Nowak <kesse8401@hotmail.com>
2018-07-03 10:30:14 +0200

    first exercise - reverse

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function myFunction(p1, p2) {
 };
 
 document.getElementById("par").innerHTML = myFunction(5, 3);
+
+// Write a JavaScript function that reverse a number
+
+function reverse() {
+    n = n + "";
+    return name.split("").reverse().join("");
+};
+
+document.getElementById("par").innerHTML = reverse(123);
