b795d8b8056b974ec0536bb00e7e1de5d57e8e2b
Agnes Nowak <kesse8401@hotmail.com>
2018-07-03 10:32:51 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function myFunction(p1, p2) {
+function test(p1, p2) {
     return p1 + p2;
 };
 
-document.getElementById("par").innerHTML = myFunction(5, 3);
+document.getElementById("a").innerHTML = test(5, 3);
 
 // Write a JavaScript function that reverse a number
 
@@ -11,4 +11,4 @@ function reverse(n) {
     return name.split("").reverse().join("");
 };
 
-document.getElementById("par").innerHTML = reverse(123);
+document.getElementById("b").innerHTML = reverse(123);
